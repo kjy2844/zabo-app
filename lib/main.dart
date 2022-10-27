fa2b4f22873796aa5ca9b729b7aae369e16b0c73
kjy2844 <kjy2844@kaist.ac.kr>
2022-10-28 01:46:40 +0900

    ✨main page

--- lib/main.dart
+++ lib/main.dart
@@ -13,103 +13,346 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Colors.black,
+        ),
+        textTheme: const TextTheme(bodyText2: TextStyle(color: Colors.black)),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: ZaboAppBar(),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Banner(),
+                MenuBar(),
+                NotiBar(),
+                PopularZabo(),
+                PopularGroup(),
+                RegisterGroup(),
+                AllZabo(),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class ZaboAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const ZaboAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return AppBar(
+      title: const Text('Zabo'),
+      elevation: 1,
+      actions: [
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text(
+            '로그인',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20.0,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(60.0);
+}
+
+class Banner extends StatelessWidget {
+  const Banner({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      margin: EdgeInsets.all(16.0),
+      child: Center(
+        child: Column(
+          children: [
+            Text('이제 포스터 확인은 자보에서,'),
+            Text('카이스트의 소식을 바로 알아보세요'),
+            Row(
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('신규 그룹 신청'),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    textStyle: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('자보 업로드'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MenuBar extends StatelessWidget {
+  const MenuBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      margin: EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(10.0),
+      child: Center(
+        child: Row(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+                  radius: 10.0,
+                  backgroundColor: Colors.black,
+                ),
+                Text('전체보기'),
+              ],
+            ),
+            Column(
+              children: [
+                CircleAvatar(
+                  radius: 10.0,
+                  backgroundColor: Colors.black,
+                ),
+                Text('행사'),
+              ],
+            ),
+            Column(
+              children: [
+                CircleAvatar(
+                  radius: 10.0,
+                  backgroundColor: Colors.black,
+                ),
+                Text('공연'),
+              ],
+            ),
+            Column(
+              children: [
+                CircleAvatar(
+                  radius: 10.0,
+                  backgroundColor: Colors.black,
+                ),
+                Text('축제'),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class NotiBar extends StatelessWidget {
+  const NotiBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      margin: EdgeInsets.all(16.0),
+      padding: EdgeInsets.all(16.0),
+      child: Center(
+        child: Text('현재 마감이 임박한 자보가 없습니다.'),
+      ),
+    );
+  }
+}
+
+class PopularZabo extends StatelessWidget {
+  const PopularZabo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const List<String> names = <String>[
+      '스팍스 2019 가을 리크루팅',
+      '학부 총학생회 신입위원 모집',
+      'DITO 참가팀 모집'
+    ];
+
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      color: Colors.grey,
+      child: Column(
+        children: [
+          Text('인기 있는 자보'),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: names.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ZaboListTile(name: names[index]);
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ZaboListTile extends StatefulWidget {
+  const ZaboListTile({
+    Key? key,
+    this.name = "",
+  }) : super(key: key);
+
+  final String name;
+
+  @override
+  State<ZaboListTile> createState() => _ZaboListTileState();
+}
+
+class _ZaboListTileState extends State<ZaboListTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(4.0),
+      child: Container(
+        child: Text(widget.name),
+        height: 30.0,
+      ),
+    );
+  }
+}
+
+class PopularGroup extends StatelessWidget {
+  const PopularGroup({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const List<String> names = <String>[
+      'Zabo Team',
+      'Idea Factory',
+      '학부 총학생회 Flex',
+      'SPARCS',
+      '산업디자인학과 학생회'
+    ];
+
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      color: Colors.grey,
+      child: Column(
+        children: [
+          Text('이 그룹은 어때요?'),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: names.length,
+            itemBuilder: (BuildContext context, int index) {
+              return GroupListTile(name: names[index]);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GroupListTile extends StatefulWidget {
+  const GroupListTile({Key? key, this.name = ""}) : super(key: key);
+
+  final String name;
+
+  @override
+  State<GroupListTile> createState() => _GroupListTileState();
+}
+
+class _GroupListTileState extends State<GroupListTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(4.0),
+      child: Container(
+        child: Text(widget.name),
+        height: 30.0,
+      ),
+    );
+  }
+}
+
+class RegisterGroup extends StatelessWidget {
+  const RegisterGroup({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      color: Colors.grey,
+      child: Column(
+        children: [
+          Text('신규 그룹 신청'),
+          Container(
+            color: Colors.black,
+            margin: EdgeInsets.all(4.0),
+            child: Container(
+              height: 30.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AllZabo extends StatelessWidget {
+  const AllZabo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16.0),
+      color: Colors.grey,
+      child: Column(
+        children: [
+          Text('전체 자보 보기'),
+          Container(
+            color: Colors.black,
+            margin: EdgeInsets.all(4.0),
+            child: Container(
+              height: 30.0,
+            ),
+          ),
+          Container(
+            color: Colors.black,
+            margin: EdgeInsets.all(4.0),
+            child: Container(
+              height: 30.0,
+            ),
+          ),
+          Container(
+            color: Colors.black,
+            margin: EdgeInsets.all(4.0),
+            child: Container(
+              height: 30.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
